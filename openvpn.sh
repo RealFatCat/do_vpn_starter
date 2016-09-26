8904778bdc623f31adbad6f670b69157efc573c4
RealFatCat <trashmessage@yandex.ru>
2016-09-26 15:03:32 +0300

    Separate to functions, add more logic

---
+++ openvpn.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+cd ~/go/bin/
+
+start_func(){
+    ./doctl compute droplet create \
+        openvpn-ready \
+        --size 512mb \
+        --image 13226822 \
+        --region nyc3 \
+        --enable-ipv6 \
+        --ssh-keys '[03:61:9c:f3:bb:09:61:9e:0b:7f:8a:5e:ac:af:05:84, 7a:14:d9:e4:ae:cf:69:ca:2f:be:a6:a1:1d:67:8e:be]'
+
+    status_func
+}
+
+stop_func(){
+    ./doctl compute droplet delete openvpn-ready
+    status_func
+}
+
+status_func(){
+    ./doctl compute droplet list
+}
+
+
+case $1 in
+    start)
+        start_func
+        ;;
+    stop)
+        stop_func
+        ;;
+    status)
+        status_func
+        ;;
+    *)
+        printf "Uknown argument"
+        ;;
+esac
