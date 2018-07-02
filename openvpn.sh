f89b2c7940e28c1c5b7a8dff44553c5ff742fb67
RealFatCat <trashmessage@yandex.ru>
2018-07-02 17:07:23 +0300

    Some fixes

--- openvpn.sh
+++ openvpn.sh
@@ -4,15 +4,13 @@ set -e
 cd ~/go/bin/
 
 start_func(){
-    ./doctl compute droplet create \
+    ./doctl -v compute droplet create \
         openvpn-ready \
         --size 512mb \
-        --image 13226822 \
+        --image 19978391 \
         --region nyc3 \
         --enable-ipv6 \
-        --ssh-keys '[03:61:9c:f3:bb:09:61:9e:0b:7f:8a:5e:ac:af:05:84, 7a:14:d9:e4:ae:cf:69:ca:2f:be:a6:a1:1d:67:8e:be]'
-
-    status_func
+        --ssh-keys '03:61:9c:f3:bb:09:61:9e:0b:7f:8a:5e:ac:af:05:84, 7a:14:d9:e4:ae:cf:69:ca:2f:be:a6:a1:1d:67:8e:be'
 }
 
 stop_func(){
